619aa1a8efff7b8aa070e18f711bb0fbbc2c0319
Michael Lustig <michael.lustig@bakkt.com>
2021-10-26 22:24:03 -0400

    Initial Commit

---
+++ QrCodeView/QrCodeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  QrCodeViewApp.swift
+//  QrCodeView
+//
+//  Created by Michael Lustig on 10/26/21.
+//
+
+import SwiftUI
+
+@main
+struct QrCodeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
